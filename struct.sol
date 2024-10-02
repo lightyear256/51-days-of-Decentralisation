d33eb09c85bb26a54bb9dee709291964bc0aef92
lightyear256 <157786533+lightyear256@users.noreply.github.com>
2024-10-02 12:12:30 +0530

    day-16

--- struct.sol
+++ struct.sol
@@ -6,3 +6,21 @@ contract Todos {
         string text;
         bool completed;
     }
+// An array of 'Todo' structs
+    Todo[] public todos;
+
+    function create(string calldata _text) public {
+        // 3 ways to initialize a struct
+        // - calling it like a function
+        todos.push(Todo(_text, false));
+
+        // key value mapping
+        todos.push(Todo({text: _text, completed: false}));
+
+        // initialize an empty struct and then update it
+        Todo memory todo;
+        todo.text = _text;
+        // todo.completed initialized to false
+
+        todos.push(todo);
+    }
