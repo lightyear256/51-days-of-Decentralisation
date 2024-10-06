905d8d9af1f8e2e9e8d7c344d8b275fdef2a723e
lightyear256 <157786533+lightyear256@users.noreply.github.com>
2024-10-06 12:12:30 +0530

    day-20

--- new_struct.sol
+++ new_struct.sol
@@ -1,8 +1,17 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.26;
+// // This is saved 'StructDeclaration.sol'
+
+// struct Todo {
+//     string text;
+//     bool completed;
+// }
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
-// This is saved 'StructDeclaration.sol'
 
-struct Todo {
-    string text;
-    bool completed;
+import "./StructDeclaration.sol";
+
+contract Todos {
+    // An array of 'Todo' structs
+    Todo[] public todos;
 }
