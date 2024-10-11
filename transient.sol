929e98506b979141f246ea6c02d6131af548880a
lightyear256 <157786533+lightyear256@users.noreply.github.com>
2024-10-11 12:20:30 +0530

    day-25

---
+++ transient.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.26;
+
+// Make sure EVM version and VM set to Cancun
+
+// Storage - data is stored on the blockchain
+// Memory - data is cleared out after a function call
+// Transient storage - data is cleared out after a transaction
+
+interface ITest {
+    function val() external view returns (uint256);
+    function test() external;
+}
