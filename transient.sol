6c7ec58295e34346dec8e5df7bc957fa801b3f48
lightyear256 <157786533+lightyear256@users.noreply.github.com>
2024-10-12 12:20:45 +0530

    day-26

--- transient.sol
+++ transient.sol
@@ -10,3 +10,14 @@ interface ITest {
     function val() external view returns (uint256);
     function test() external;
 }
+contract Callback {
+    uint256 public val;
+
+    fallback() external {
+        val = ITest(msg.sender).val();
+    }
+
+    function test(address target) external {
+        ITest(target).test();
+    }
+}
