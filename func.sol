3f0a46b888962b5d2f1fae93a976a58ed79486bf
lightyear256 <157786533+lightyear256@users.noreply.github.com>
2024-10-20 21:01:16 +0530

    day-34

--- func.sol
+++ func.sol
@@ -10,3 +10,10 @@ contract Function {
     function named() public pure returns (uint256 x, bool b, uint256 y) {
         return (1, true, 2);
     }
+    // Return values can be assigned to their name.
+    // In this case the return statement can be omitted.
+    function assigned() public pure returns (uint256 x, bool b, uint256 y) {
+        x = 1;
+        b = true;
+        y = 2;
+    }
